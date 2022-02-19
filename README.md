afffd7eb1a4340e4750898d4a5fe40925e8bbe58
Mosiah Hazen <haz17005@byui.edu>
2022-02-18 23:51:45 -0700

    Finished Patron Table

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# SQLDataBase
-Creating an equipment lender program for practice with SQL Data Bases
+# Overview
+
+This is a simple SQL based database that would be used for an equipment lending company. The program open up a menu in the terminal that will allow 
+the user to navigate to the table they wish to work with. Once they do the program will then prompt the user to indicate a command that they wish to preform with the tables. The databases are built in SQLite3 which is a sort of local SQL database.
+
+I wrote this program to get practice using SQL Relational Databases. I chose to specifically make a "Equipment Lender" database because I use a 
+database like this everyday at my work with the BYUI Production Office, but recently we have run into a few problems with this database and software
+so thought it would be interesting to "mock up" a replacement software and database. 
+
+[Software Demo Video](https://youtu.be/cvBuPgGyoDw)
+
+# Relational Database
+
+Like I wrote a bit earlier, I created this program using SQLite3. I would say the biggest reason I did this was so that I could practice SQL syntax, 
+
+My program currently is divided into the maintenece of two tables. One for equipment the other for patrons. They both include Names and unique ID's, 
+the unique ID's are based off the use of barcodes and card scans that we use currently at my office. This allows for there to be two patrons or pieces of equipment that have the same name, but are different and are treated differently.
+
+# Development Environment
+
+I used VS Code to edit my code. I like it because it has a lot of Python functionality built in, which makes it much easier and faster to develop Python programs.
+
+I used Python to access SQLite through the sqlite3 library. I also used the connection library from multiprocessing to actually connect to my database.
+
+# Useful Websites
+
+* [SQLiteTutorial](https://www.sqlitetutorial.net/)
+* [Python](https://docs.python.org/3.8/library/sqlite3.html)
+* [Educba](https://www.educba.com/sqlite-sum/)
+
+# Future Work
+
+* Item 1 - I want to make a third table that is named reservations. This table will be able to access both other databases to make reservations for patrons of equipment. This table would check to make sure the status and condition of patrons and resources were viable before allowing for reservations to be made. It would also hold date and time information about the length of the reservation and due dates. Finally it would keep the total value of all the equipment that the reservation holds.
+* Item 2 - I want to add more checks to make sure that the user is inputting valid data that will not cause the program to crash.
+* Item 3 - I want to eventually make the menu in an app, or some sort of User Interface that will make the program more user friendly and appealing to use.1
--- sql_data_base.py
+++ sql_data_base.py
@@ -17,22 +17,39 @@ cursor = connection.cursor()
 
 # Creating a table if there is not one.
 cursor.execute("CREATE TABLE IF NOT EXISTS resources(item_name TEXT, item_id TEXT, barcode REAL, value REAL, condition TEXT)")
-cursor.execute("CREATE TABLE IF NOT EXISTS patrons(patron_name TEXT, patron_id_number REAL, phone_number TEXT, email TEXT, standing TEXT)")
+cursor.execute("CREATE TABLE IF NOT EXISTS patrons(patron_name TEXT, patron_id_number INTEGER, phone_number TEXT, email TEXT, standing TEXT)")
 
-
+# First menu user sees.
 def display_menu():
+    print("1) Equipment Table")
+    print("2) Patron Table")
+    print("3) Quit")
+
+# If they navigate to the equipment menu.
+def display_equip_menu():
     print("1) Display Equipment")
     print("2) Add Equipment")
     print("3) Update Condition")
     print("4) Delete Equipment")
-    print("5) Quit")
+    print("5) Display Total Value")
+    print("6) Back to Main")
 
+# If they navigate to the patron menu.
+def display_patron_menu():
+    print("1) Display Patrons")
+    print("2) Add Patron")
+    print("3) Display Contact Info")
+    print("4) Delete Patron")
+    print("5) Back to Main")
+
+# Simple query to display all the resources in the table resources.
 def display_equipment():
     cursor.execute("SELECT * FROM resources")
     print("{:>14}  {:>17}  {:>16}  {:>16}".format("Name", "ID", "Value", "Condition"))
     for record in cursor.fetchall():
         print("{:>20}  {:>14}  {:>14}  {:>15}".format(record[0], record[1], record[3], record[4]))
 
+# Inserting rows into the table resources.
 def add_equipment():
     equipment_name = input("Name > ")
     equipment_id = input("Unique ID > ")
@@ -43,6 +60,8 @@ def add_equipment():
     cursor.execute("INSERT INTO resources VALUES (?, ?, ?, ?, ?)", values)
     connection.commit()
 
+# Querying the resources table with a unique value, then updating a
+# specific column.
 def update_condition():
     unique_identifier = input("ID or Barcode > ")
     values1 = (unique_identifier, unique_identifier)
@@ -53,25 +72,57 @@ def update_condition():
     cursor.execute("UPDATE resources SET condition = ? WHERE item_id = ? OR barcode = ?", values)
     connection.commit()
 
+# Removing a specific item.
 def delete_equipment():
     unique_identifier = input("ID or Barcode > ")
     values1 = (unique_identifier, unique_identifier)
     cursor.execute("DELETE FROM resources WHERE item_id = ? OR barcode = ?", values1)
     connection.commit()
 
+# Summing the value of all resources.
+def total_value():
+    cursor.execute("SELECT SUM(value) FROM resources")
+    total = cursor.fetchone()[0]
+    print(f"Total Value of Center: {total}")
+
+# Inserting a row into patron table.
 def add_patron():
-    print
+    patron_name = input("Name > ")
+    patron_id_number = input("Unique ID Number > ")
+    phone_number = input("Phone Number > ")
+    email = input("Email > ")
+    status = input("Status > ")
+    values = (patron_name, patron_id_number, phone_number, email, status)
+    cursor.execute("INSERT INTO patrons VALUES (?, ?, ?, ?, ?)", values)
+    connection.commit()
 
+# Querying the patron table for all rows.
 def display_patrons():
-    print
+    cursor.execute("SELECT * FROM patrons")
+    print("{:>14}  {:>11}".format("Name", "ID Number"))
+    for record in cursor.fetchall():
+        print("{:>14}  {:>11}".format(record[0], int(record[1])))
 
+# Querying the patron table for specific information that could be
+# useful in real life.
 def show_contact_info():
+    patron_id_number = input("ID Number > ")
+    cursor.execute("SELECT * FROM patrons WHERE patron_id_number = ?", (patron_id_number,))
+    patron = cursor.fetchall()[0]
     print
+    print("Contact Info for {}: {} {}".format(patron[0], patron[2], patron[3]))
 
-def main():
+# Deleting a row from patron table.
+def delete_patron():
+    patron_id_number = input("ID Number > ")
+    cursor.execute("DELETE FROM patrons WHERE patron_id_number = ?", (patron_id_number,))
+    connection.commit()
+
+# Prompts user for built in command.
+def equipment_menu():
     userInput = NULL
-    while(userInput != 5):
-        display_menu()
+    while(userInput != 6):
+        display_equip_menu()
         userInput = int(input("> "))
         print()
         match userInput:
@@ -83,6 +134,45 @@ def main():
                 update_condition()
             case 4:
                 delete_equipment()
+            case 5:
+                total_value()
+
+        print()
+    return None
+
+# Prompts user for built in command.
+def patron_menu():
+    userInput = NULL
+    while(userInput != 5):
+        display_patron_menu()
+        userInput = int(input("> "))
+        print()
+        match userInput:
+            case 1:
+                display_patrons()
+            case 2:
+                add_patron()
+            case 3:
+                show_contact_info()
+            case 4:
+                delete_patron()
+
+        print()
+    return None
+
+# Calls other functions to get the program started and also to end 
+# it.
+def main():
+    userInput = NULL
+    while(userInput != 3):
+        display_menu()
+        userInput = int(input("> "))
+        print()
+        match userInput:
+            case 1:
+                equipment_menu()
+            case 2:
+                patron_menu()
 
         print()
     connection.close()
